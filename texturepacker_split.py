faa3da92e3d30959c63558e1155334e249e87375
GTEVO <gteavarro@gmail.com>
2019-05-13 11:27:05 +0800

    编码

--- texturepacker_split.py
+++ texturepacker_split.py
@@ -1,7 +1,6 @@
-#!python v2.7
-# 1? ??? PIL ?
-# 2? ?????????????png?plist?????????
-# 3? pvr?????????texturepacker tool/view ?????png??
+#!python v2.7 encode format : Western(ISO 8859-15)
+# 1?   ?? PIL
+# 2?   ??png?plist?????
 import os,sys
 from xml.etree import ElementTree
 from PIL import Image
@@ -27,7 +26,7 @@ def gen_png_from_plist(plist_filename, png_filename):
     plist_dict = tree_to_dict(root[0])
     to_list = lambda x: x.replace('{','').replace('}','').split(',')
     for k,v in plist_dict['frames'].items():
-        #???????Rect
+        # ?????? Rect
         rectlist = to_list(v['frame'])
         rect_w = rectlist[2]
         rect_h = rectlist[3]
@@ -43,10 +42,10 @@ def gen_png_from_plist(plist_filename, png_filename):
             int(rectlist[0]) + rect_w,
             int(rectlist[1]) + rect_h,
             )
-        #???? ??? ??
+        # ?????? ??
         rect_on_big = big_image.crop(rect)
         if is_rotated:
-            #??? ???????????????????????????
+            # ?????????....
             side_length = max(rect_w, rect_h)
             temp_image = Image.new('RGBA',(side_length, side_length),(0,0,0,0))
             temp_x = side_length / 2 - rect_w / 2
@@ -69,11 +68,11 @@ def gen_png_from_plist(plist_filename, png_filename):
             )
             rect_on_big = rect_on_big.crop(temp_box)
 
-        #????????????????????
+        # ?????
         sizelist = [ int(x) for x in to_list(v['sourceSize'])]
         result_image = Image.new('RGBA', sizelist, (0,0,0,0))
         
-        #??????? Rect
+        # ?????
         source_color_rect = to_list(v['sourceColorRect'])
         result_rect = (
             int(source_color_rect[0]),
@@ -92,8 +91,8 @@ def gen_png_from_plist(plist_filename, png_filename):
         if not os.path.isdir(file_path):
             os.mkdir(file_path)
         outfile = (file_path+'/' + k).replace('gift_', '')
-        print outfile, "generated succeessfully"
-        print '------------------------------------------------------------------------------------'
+        #print outfile, "generated succeessfully"
+        #print '------------------------------------------------------------------------------------'
         result_image.save(outfile)
 
 if __name__ == '__main__':
@@ -103,7 +102,7 @@ if __name__ == '__main__':
             if os.path.splitext(filename)[1] == '.plist':
                 name = os.path.splitext(filename)[0]
                 plist_filename = filename 
-                png_filename = name + '.png'
+                png_filename = name + '.pvr.png'
                 print 'Start To Split Png :' + png_filename
                 if (os.path.exists(plist_filename) and os.path.exists(png_filename)):
                     gen_png_from_plist( plist_filename, png_filename )
